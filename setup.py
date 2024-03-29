6d49d0a4faf5db4b8427a0077b2f7adecd8cf816
PravinSK <pravinsk87@gmail.com>
2024-03-29 15:49:48 +0530

    required library added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages,setup
+
+
+
+setup(
+    name="Generative AI Project",
+    version = '0.0.0',
+    author="Pravin",
+    author_email="pravin@gmail.com",
+    packages=find_packages(),
+    install_requires = []
+)
